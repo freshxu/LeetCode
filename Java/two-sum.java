08d0de83f7f6b3c5284cdd547956acb51f9770eb
freshxu <freshxu110@163.com>
2018-07-17 10:55:51 +0800

    addjust code format

--- Java/two-sum.java
+++ Java/two-sum.java
@@ -1,15 +1,13 @@
-class Solution {
-	public int[] twoSum( int[] nums, int target )
-	{
-		Map<Integer, Integer> map = new HashMap<>();
-		for ( int i = 0; i < nums.length; i++ )
-		{
-			if ( map.containsKey( target - nums[i] ) )
-			{
-				return(new int[] { i, map.get( target - nums[i] ) });
-			}
-			map.put( nums[i], i );
-		}
-		throw new IllegalArgumentException( "No two sum solution" );
-	}
-}
+ class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (map.containsKey(target - nums[i])) {
+                    return (new int[]{i, map.get(target - nums[i])});
+                }
+                map.put(nums[i], i);
+            }
+            throw new IllegalArgumentException("No two sum solution");
+        }
+    }
+    
--- Kotlin/two-sum.kt
+++ Kotlin/two-sum.kt
@@ -1,9 +1,9 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-      val map : HashMap<Int, Int> = hashMapOf()
-        for (key in  nums.indices) {
+      val map: HashMap<Int, Int> = hashMapOf()
+        for (key in nums.indices) {
             if (map.containsKey(target - nums[key])) {
-                val resultArray : IntArray = intArrayOf(map.get(target - nums[key]) ?: 0,key)
+                val resultArray: IntArray = intArrayOf(map.get(target - nums[key]) ?: 0,key)
                 return resultArray
             }
             map.put(nums[key],key)
