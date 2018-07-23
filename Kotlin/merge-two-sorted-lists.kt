68124060b580f050efd0b27e025426d73a775271
freshxu <freshxu110@163.com>
2018-07-23 23:13:39 +0800

    add merge-two-sorted-lists.kt solution

---
+++ Kotlin/merge-two-sorted-lists.kt
@@ -0,0 +1,13 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2
+        if (l2 == null) return l1
+        if (l1.var <= l2.var) {
+            l1.next = mergeTwoLists(l1.next, l2)
+            return l1
+        } else {
+            l2.next = mergeTwoLists(l2.next, l1)
+            return l2
+        }
+    }
+}
