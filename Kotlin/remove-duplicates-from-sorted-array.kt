7a20bbf64016ca75076569609991db0b2de170a4
freshxu <freshxu110@163.com>
2018-07-24 23:07:38 +0800

    update ： add remove-duplicates-from-sorted-array.kt

---
+++ Kotlin/remove-duplicates-from-sorted-array.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun removeDuplicates(nums: IntArray): Int {
+        if (nums.size == 0 || nums.size == 1) return nums.size
+        var i = 0
+        var j = 1
+        var newSize = 1
+        while (j < nums.size) {
+            if (nums[i] < nums[j]) {
+                nums[i + 1] = nums[j]
+                i ++
+                j ++
+                newSize ++
+            } else {
+                j++
+            }
+        }
+        return newSize
+    }
+}
