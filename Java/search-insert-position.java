c118adf449331bd668af4fafa9584b3fc70bec68
freshxu <freshxu110@163.com>
2018-07-27 18:18:58 +0800

    update search-insert-position.java add second solution

--- Java/search-insert-position.java
+++ Java/search-insert-position.java
@@ -20,4 +20,31 @@ class Solution {
         }
         return -1;
     }
+
+    // solution 2
+
+    public int searchInsert(int[] nums, int target) {
+    
+        if (target < nums[0]) {
+            return 0;
+        }
+        if (target > nums[nums.length - 1]) {
+            return nums.length;
+        }
+        
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int middle = (start + end) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] < target) {
+                start = middle + 1;
+            } else {
+                end = middle;
+            }
+        }
+        return start;
+    }
+    
 }
