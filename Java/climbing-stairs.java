87d4a000ddb67c64d5af21bd4356f7e37d61f754
freshxu <freshxu110@163.com>
2018-08-03 20:37:06 +0800

    add climbing-stairs.java solution

---
+++ Java/climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int n1 = 1;
+        int n2 = 2;
+        for (int i = 3; i <= n; i++) {
+            int temp = n2;
+            n2 = n1 + n2;
+            n1 = temp;
+        }
+        return n2;
+    }
+}
+
