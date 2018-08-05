7c1108b84071d43f0e7a63ab082f695a5598ca25
freshxu <freshxu110@163.com>
2018-08-06 00:05:01 +0800

    add symmetric-tree.java solution

---
+++ Java/symmetric-tree.java
@@ -0,0 +1,22 @@
+/**
+ *  * Definition for a binary tree node.
+ *   * public class TreeNode {
+ *    *     int val;
+ *     *     TreeNode left;
+ *      *     TreeNode right;
+ *       *     TreeNode(int x) { val = x; }
+ *        * }
+ *         */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return isSymmetric(root.left, root.right);
+    }
+    public static boolean isSymmetric(TreeNode left, TreeNode right) {
+        if(left == null && right == null) return true;
+        if(left == null || right == null) return false;
+        if(left.val == right.val) return isSymmetric(left.left, right.right)
+        		&& isSymmetric(left.right, right.left);
+        return false;
+    }
+}
