4ecc311d2f952c93fc3fe942e135cd5991caafb5
freshxu <freshxu110@163.com>
2018-08-28 00:06:30 +0800

    adjust code and name

--- Java/excel-sheet-column-title.java
+++ Java/excel-sheet-column-title.java
@@ -1,8 +1,8 @@
 public String convertToTitle(int n) {
         StringBuilder sb = new StringBuilder();
         while (n > 0) {
-            sb.insert(0, (char)((n - 1) % 26 + 'A'));  //①
-            n = (n - 1) / 26;  //②
+            sb.insert(0, (char)((n - 1) % 26 + 'A'));
+            n = (n - 1) / 26;
         }
         return sb.toString();
     }
