8dbb5a4fed93f420c6f3cc5efc23b9dec20c3ae2
freshxu <freshxu110@163.com>
2018-08-28 23:55:01 +0800

    add majority-element.java solution

---
+++ Java/majority-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int res = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                res = num;
+                count++;
+            } else if (res == num) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return res;
+    }
+}
