22e86c19e7a2c3ca41c6e1c7974cb47b22e675e3
freshxu <freshxu110@163.com>
2018-09-03 23:38:18 +0800

    add contains-duplicate solution2

--- Java/contains-duplicate.java
+++ Java/contains-duplicate.java
@@ -8,4 +8,17 @@ class Solution {
         }
         return false;
     }
+
+    // solution 2
+    public boolean containsDuplicate(int[] nums) {
+       HashSet set = new HashSet();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        if (set.size() != nums.length) {
+            return true;
+        } 
+        return false;
+    }
+    
 }
