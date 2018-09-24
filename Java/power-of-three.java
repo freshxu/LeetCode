c1b17e891ecbda5e0c7ac04ea81899d1cf6f5110
freshxu <freshxu110@163.com>
2018-09-24 23:46:21 +0800

    add power-of-three.java solution

---
+++ Java/power-of-three.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) return false;
+        while (n != 1) {
+            if (n % 3 != 0) {
+                return false;
+            } else {
+                n /= 3;
+            }
+        }
+        return true;
+    }
+}
