a4f51f2c7a3b6dfac3ad0c57da80cd1a64822755
freshxu <freshxu110@163.com>
2018-09-27 19:41:39 +0800

    add reverse-string.java solution

---
+++ Java/reverse-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String reverseString(String s) {
+        char[] chars = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            sb.append(chars[i]);
+        }
+        return sb.toString();
+    }
+}
