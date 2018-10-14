4312bfedc846db60bf36687a2bc518fefcf094ca
freshxu <freshxu110@163.com>
2018-10-14 23:55:06 +0800

    add first-unique-character-in-a-string.java solution

---
+++ Java/first-unique-character-in-a-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] chars = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            chars[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (chars[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
