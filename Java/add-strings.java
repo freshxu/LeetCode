2da2cc7cc58f34ba99e37d7340369dd83106aefd
freshxu <freshxu110@163.com>
2018-10-24 23:54:53 +0800

    add add-strings.java solution

---
+++ Java/add-strings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        StringBuilder builder = new StringBuilder();
+        int carry = 0;
+        while (i >= 0 || j >= 0) {
+            if (i >= 0) {
+                carry += num1.charAt(i) - '0';
+            }
+            if (j >= 0) {
+                carry += num2.charAt(j) - '0';
+            }
+            builder.append((char)(carry % 10 + '0'));
+            carry /= 10;
+            i--;
+            j--;
+        }
+        if (carry == 1) {
+            builder.append('1');
+        }
+        return builder.reverse().toString();
+    }
+}
